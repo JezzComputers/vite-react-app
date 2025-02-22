300f061bef152dcbbb92ee234cd0b87cc4f36cc9
JezzComputers <jeremy_sprague@outlook.com>
2025-02-22 21:48:23 +1100

    added TicTacToe

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,17 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./TicTacToe-style.css";
+
+import TicTacToe from "./TicTacToe";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <TicTacToe />
+  </StrictMode>
+);
